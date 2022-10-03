89bfd6a55e21275ad38b79b71b42b5e30f2bc69f
Justin Junious <justinjunious@gmail.com>
2022-10-03 16:09:13 -0400

    added more buttons for different play options and updated global variables

--- playgame.js
+++ playgame.js
@@ -1,21 +1,32 @@
 // Global Variables
 let gameButton = document.querySelector('.game button')
 let gameChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let playerCards = document.getElementsByClassName('p1')
+let pCards = document.getElementsByClassName('p1')
 let opponentCards = document.getElementsByClassName('.opp')
-// playerCards.forEach(players(,))
+let winner = document.getElementsByClassName('.winner')
+
+// let array = [a, b, c, d]
+// const a = 1
+// const b = 2
+// const c = 3
+// const d = 4
 
 // event listeners
-let click1 = gameButton.addEventListener('click', () => {
-  console.log('button clicked')
-  if (playerCards.src === '') {
-    playerCards.src = 'https://pngimg.com/uploads/cards/cards_PNG8492.png'
-  }
+let gameStart = gameButton.addEventListener('click', () => {
+  gameButton.innerHTML = 'HIT'
+  // const choices = pCards.forEach((i) => {
+  //   if (pCards.src === '') {
+  //     pCards[i].src = 'https://pngimg.com/uploads/cards/cards_PNG8492.png'
+  //   }
+  console.log(choices)
 })
 
 //Variables for card PNG
 
 // Winning conditions
+if (sum(gameChoices) === 21) {
+  winner.innerHTML = 'You WIN'
+}
 
 //Functions
 
