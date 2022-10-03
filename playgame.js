4ad7522003ee4f7c5c12a7303b76a4b27eec69f7
Justin Junious <justinjunious@gmail.com>
2022-10-03 14:27:27 -0400

    update event listeners and modified buttons

--- playgame.js
+++ playgame.js
@@ -1,13 +1,16 @@
 // Global Variables
-let player = document.querySelector('.player button')
-let opponent = document.querySelector('.opponent button')
+let gameButton = document.querySelector('.game button')
+let gameChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let playerCards = document.getElementsByClassName('p1')
+let opponentCards = document.getElementsByClassName('.opp')
+// playerCards.forEach(players(,))
 
 // event listeners
-let click1 = player.addEventListener('click', () => {
-  console.log('click worked')
-})
-opponent.addEventListener('click', () => {
-  console.log('click worked')
+let click1 = gameButton.addEventListener('click', () => {
+  console.log('button clicked')
+  if (playerCards.src === '') {
+    playerCards.src = 'https://pngimg.com/uploads/cards/cards_PNG8492.png'
+  }
 })
 
 //Variables for card PNG
