dfe606578e7764ec6224c1c169170a275bfb84a6
Justin Junious <justinjunious@gmail.com>
2022-10-03 12:01:35 -0400

    working on javascript and event listeners

---
+++ playgame.js
@@ -0,0 +1,20 @@
+// Global Variables
+let player = document.querySelector('player button')
+let opponent = document.querySelector('opp button')
+
+//Event listeners
+
+let click1 = player.addEventListener('click', () => {
+  console.log('click worked')
+})
+opponent.addEventListener('click', () => {
+  console.log('click worked')
+})
+
+//Variables for card PNG
+
+// Winning conditions
+
+//Functions
+
+//Game logic
