a54282e92cd78713557ba44a41f48ff5d1f63a1a
Justin Junious <justinjunious@gmail.com>
2022-10-04 10:31:21 -0400

    added more global variables for interact buttons, random number generator and working on hit logic

--- playgame.js
+++ playgame.js
@@ -1,9 +1,18 @@
 // Global Variables
 let gameButton = document.querySelector('.game button')
+let playTurn = document.querySelector('.Hit')
 let gameChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let pCards = document.getElementsByClassName('p1')
-let opponentCards = document.getElementsByClassName('.opp')
+let opponentCards = document.getElementsByClassName('opp')
 let winner = document.getElementsByClassName('.winner')
+let player = 1
+
+// functions
+let randomChoices = () => {
+  const choice = gameChoices[Math.floor(Math.random() * gameChoices.length)]
+  const choices = choice
+  return choices
+}
 
 // let array = [a, b, c, d]
 // const a = 1
@@ -13,20 +22,34 @@ let winner = document.getElementsByClassName('.winner')
 
 // event listeners
 let gameStart = gameButton.addEventListener('click', () => {
-  gameButton.innerHTML = 'HIT'
-  // const choices = pCards.forEach((i) => {
-  //   if (pCards.src === '') {
-  //     pCards[i].src = 'https://pngimg.com/uploads/cards/cards_PNG8492.png'
-  //   }
-  console.log(choices)
+  pCards[0].innerHTML = randomChoices()
+  pCards[1].innerHTML = randomChoices()
+  opponentCards[0].innerHTML = randomChoices()
+  gameButton.innerHTML = 'Stay'
+  playTurn.style.display = 'block'
+  let player = 1
+  gameButton.removeEventListener('click', gameStart)
+})
+
+let gamePlay = playTurn.addEventListener('click', () => {
+  let i = 0
+  if (player === 1 && pCards[i] === 0) {
+    for (i < pCards.length; i++; ) {
+      pCards[i].innerHTML = randomChoices()
+    }
+    if (gameButton === 'click') {
+    }
+  } else {
+    console.log('yay')
+  }
 })
 
 //Variables for card PNG
 
 // Winning conditions
-if (sum(gameChoices) === 21) {
-  winner.innerHTML = 'You WIN'
-}
+// if () === 21) {
+//   winner.innerHTML = 'You WIN'
+// }
 
 //Functions
 
